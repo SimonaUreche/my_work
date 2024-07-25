7009155614f3a44e514ed0bc7ddc2b44c49da670
SimonaUreche <146859121+SimonaUreche@users.noreply.github.com>
2024-07-25 14:25:34 +0300

    Delete Orders management directory

--- Orders management/java/dao/ClientDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.Client;
-
-/**
- * Clasa pentru accesul la datele tabelei 'client' din baza de date, specializată pentru entitatea Client.
- */
-public class ClientDAO extends AbstractDAO<Client> {
-}
