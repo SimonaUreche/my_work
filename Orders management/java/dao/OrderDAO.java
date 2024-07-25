4123f7c1ceaf01364e1a01fa287054bd5139a1ad
SimonaUreche <146859121+SimonaUreche@users.noreply.github.com>
2024-07-25 14:26:16 +0300

    Queue Management Application

--- Orders management/java/dao/OrderDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.Orders;
-
-/**
- * Clasa pentru accesul la datele tabelei 'orders' din baza de date, specializată pentru entitatea Orders.
- */
-public class OrderDAO extends AbstractDAO<Orders> {
-}
--- Orders management/java/dao/ProductDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.Product;
-
-/**
- * Clasa pentru accesul la datele tabelei 'product' din baza de date, specializată pentru entitatea Product.
- */
-public class ProductDAO extends AbstractDAO<Product> {
-}
